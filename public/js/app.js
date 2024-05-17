b6d6ce598ee1bf3588e6fd54a216855b00c41a9c
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-05-18 04:55:50 +0600

    Поправил маленька

--- public/js/app.js
+++ public/js/app.js
@@ -1,132 +1,102 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const btn1 = document.getElementById("btn1")
-    const lng1 = document.getElementById("lng1")
+    const toggleClass = (buttonId, elementId, className) => {
+        const button = document.getElementById(buttonId);
+        const element = document.getElementById(elementId);
 
-    btn1.addEventListener("click", function () {
-        lng1.classList.toggle("language-active")
-    })
-    document.addEventListener("click", function () {
-        if (!btn1.contains(event.target) && event.target !== lng1) {
-            lng1.classList.remove("language-active")
-        }
-    })
+        button.addEventListener("click", () => {
+            element.classList.toggle(className);
+        });
 
-    const btn2 = document.getElementById("btn2")
-    const lng2 = document.getElementById("lng2")
+        document.addEventListener("click", (event) => {
+            if (!button.contains(event.target) && event.target !== element) {
+                element.classList.remove(className);
+            }
+        });
+    };
 
-    btn2.addEventListener("click", function () {
-        lng2.classList.toggle("language-active")
-    })
-    document.addEventListener("click", function () {
-        if (!btn2.contains(event.target) && event.target !== lng2) {
-            lng2.classList.remove("language-active")
-        }
-    })
+    toggleClass("btn1", "lng1", "language-active");
+    toggleClass("btn2", "lng2", "language-active");
+    toggleClass("button1", "language1", "language-active");
+    toggleClass("button2", "language2", "language-active");
 
-    const button1 = document.getElementById("button1")
-    const language1 = document.getElementById("language1")
+    const account = document.getElementById("account");
+    const account1 = document.getElementById("account1");
+    const close = document.getElementById("close");
+    const auth = document.getElementById("auth");
+    const over = document.getElementById("over");
+    const fake = document.getElementById("real-fake");
 
-    button1.addEventListener("click", function () {
-        language1.classList.toggle("language-active")
-    })
-    document.addEventListener("click", function () {
-        if (!button1.contains(event.target) && event.target !== language1) {
-            language1.classList.remove("language-active")
-        }
-    })
+    const toggleAuth = (accountButton) => {
+        accountButton.addEventListener("click", () => {
+            auth.classList.toggle("auth-window-active");
+            over.classList.toggle("over-active");
+            fake.classList.toggle("real-fake-active");
+        });
+    };
 
-    const button2 = document.getElementById("button2")
-    const language2 = document.getElementById("language2")
+    const closeAuth = (closeButton) => {
+        closeButton.addEventListener("click", () => {
+            auth.classList.remove("auth-window-active");
+            over.classList.remove("over-active");
+            fake.classList.remove("real-fake-active");
+        });
+    };
 
-    button2.addEventListener("click", function () {
-        language2.classList.toggle("language-active")
-    })
-    document.addEventListener("click", function () {
-        if (!button2.contains(event.target) && event.target !== language2) {
-            language2.classList.remove("language-active")
-        }
-    })
+    toggleAuth(account);
+    toggleAuth(account1);
+    closeAuth(close);
 
-    const account = document.getElementById("account")
-    const account1 = document.getElementById("account1")
-    const close = document.getElementById("close")
-    const auth = document.getElementById("auth")
-    const over = document.getElementById("over")
+    const btn_reg = document.getElementById("btn-reg");
+    const btn_auth = document.getElementById("btn-auth");
+    const reg = document.getElementById("reg");
+    const close_reg = document.getElementById("close-reg");
 
-
-    account.addEventListener("click", function () {
-        auth.classList.toggle("auth-window-active")
-    })
-    if (!account.contains(event.target) && event.target !== auth) {
-        auth.classList.remove("auth-window-active")
-    }
-    close.addEventListener("click", function () {
-        auth.classList.remove("auth-window-active")
-    })
-
-    account1.addEventListener("click", function () {
-        auth.classList.toggle("auth-window-active")
-    })
-    if (!account1.contains(event.target) && event.target !== auth) {
-        auth.classList.remove("auth-window-active")
-    }
-    account1.addEventListener("click", function () {
-        over.classList.toggle("over-active")
-    })
-
-    account.addEventListener("click", function () {
-        over.classList.toggle("over-active")
-    })
-    if (!account.contains(event.target) && event.target !== auth) {
-        auth.classList.remove("auth-window-active")
-    }
-    close.addEventListener("click", function () {
-        over.classList.remove("over-active")
-    })
-
-    const btn_reg = document.getElementById("btn-reg")
-    const btn_auth = document.getElementById("btn-auth")
-    const reg = document.getElementById("reg")
-    const close_reg = document.getElementById("close-reg")
-
-    btn_reg.addEventListener("click", function () {
-        reg.classList.toggle("reg-window-active")
-    })
-    btn_reg.addEventListener("click", function () {
-        auth.classList.remove("auth-window-active")
-    })
-    if (!btn_reg.contains(event.target) && event.target !== reg) {
-        reg.classList.remove("reg-window-active")
-    }
-    close_reg.addEventListener("click", function () {
-        reg.classList.remove("reg-window-active")
-    })
-    close_reg.addEventListener("click", function () {
-        over.classList.remove("over-active")
-    })
-
-    btn_auth.addEventListener("click", function () {
-        reg.classList.remove("reg-window-active")
-    })
-    btn_auth.addEventListener("click", function () {
-        auth.classList.toggle("auth-window-active")
-    })
-    if (!btn_auth.contains(event.target) && event.target !== auth) {
-        auth.classList.remove("auth-window-active")
-    }
-    close.addEventListener("click", function () {
-        over.classList.remove("over-active")
-    })
-
-    var ru_lan = document.getElementById("ru-lan");
-    var button_language = document.getElementById("button-language");
-    var ru_cnt = document.getElementById("ru-cnt");
-    var button_country = document.getElementById("button-country");
-
-    ru_lan.addEventListener("click", function () {
-        button_language.innerHTML = "Русский";
+    btn_reg.addEventListener("click", () => {
+        reg.classList.toggle("reg-window-active");
+        auth.classList.remove("auth-window-active");
     });
-    ru_cnt.addEventListener("click", function () {
-        button_country.innerHTML = "Россия";
+
+    btn_auth.addEventListener("click", () => {
+        auth.classList.toggle("auth-window-active");
+        reg.classList.remove("reg-window-active");
     });
-})
+
+    close_reg.addEventListener("click", () => {
+        reg.classList.remove("reg-window-active");
+        over.classList.remove("over-active");
+        fake.classList.remove("real-fake-active");
+    });
+
+    const updateTextContent = (buttonId, targetId, text) => {
+        const button = document.getElementById(buttonId);
+        const target = document.getElementById(targetId);
+
+        button.addEventListener("click", () => {
+            target.innerHTML = text;
+        });
+    };
+
+    updateTextContent("ru-lan", "button-language", "Русский");
+    updateTextContent("ru-cnt", "button-country", "Россия");
+});
+
+// const apiUrl = "http://92.124.138.138:7000/api/";
+const apiUrl = "http://localhost:7000/api";
+
+const fetchLanguages = async () => {
+    const response = await fetch(`${apiUrl}/languages`);
+    const languages = await response.json();
+
+    // console.log(languages);
+
+    const languagesList = document.getElementById("lng1");
+    languagesList.innerHTML = "";
+
+    languages.forEach((language) => {
+        const listItem = document.createElement("tr");
+        listItem.innerHTML = `<li><a href="#">${language.Name}</a></li>`;
+        languagesList.appendChild(listItem);
+    });
+};
+
+fetchLanguages();
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const server = express();
 const path = require('path');
+const cors = require('cors');
 
 server.use('/public', express.static(path.join(__dirname, 'public')));
+server.use(cors())
 
 server.get('/', function (req, res) {
     res.sendfile('index.html');
