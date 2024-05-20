8f6b1264049be1f073bf8763723884f68dbed4e4
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-05-20 17:54:00 +0600

    Добавил вывод постов и установил порт 9000

--- src/services/axios-instance.js
+++ src/services/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:7000',
+    baseURL: 'https://news-4-you-backend.ru',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json',
