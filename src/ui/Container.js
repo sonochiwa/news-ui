e22a0c9dffa07c746558bea591fa023fdf89db25
Рашид Мамаримов <sonochiwaxd@gmail.com>
2024-06-09 04:20:29 +0600

    Подправил стили для мобилки

--- src/ui/Container.js
+++ src/ui/Container.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    //min-width: 760px;
     min-width: 340px;
     
     width: 100%;
@@ -9,12 +8,12 @@ const Container = styled.div`
     margin: 0 auto;
     padding: 0 15px;
 
-    @media (max-width: 500px) {
-        padding: 0 0;
-    }
-
     @media (max-width: 700px) {
         padding: 0 10px 0 5px;
+    }
+    
+    @media (max-width: 500px) {
+        padding: 0 0;
     }
 `
 
